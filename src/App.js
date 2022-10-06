109c6a4a92d28ec7d59ed08e71a5add0c813b15c
wangare <marywangareee@gmail.com>
2022-10-06 21:19:44 +0300

    navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Navbar from './nurucare'
+import './nurucare.css'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+export default App
---
+++ src/nurucare.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './nurucare.css'
+
+const Navbar=()=>{
+    return(
+        <body>
+            <div className="nav">
+                <a href="#">Home</a>
+                <a href="#about us">About</a>
+                <a href="#contact">Contact</a>
+                <div className='logo'></div>
+            </div>
+        </body>
+    )
+}
+export default Navbar
